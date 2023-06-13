4298b9ce60be09494b99062da2e62b5bb10bc9f7
lazych <redouancherrat@gmail.com>
2023-06-13 03:41:13 +0100

    setting up auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,25 @@
-import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
-import "./App.css"
+import { SignedIn, SignedOut, UserButton, useClerk, useAuth, useUser } from "@clerk/clerk-react"
 
-function App() {
-  const [count, setCount] = useState(0)
+function SignInButton() {
+  const clerk = useClerk()
 
+  return <button onClick={() => clerk.openSignIn({})}>Sign in</button>
+}
+
+function Header() {
   return (
-    <>
-      <div>
-        <div className={`text-3xl bg-red-400 font-bold`}>testing</div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <header>
+      <nav>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+
+        <SignedIn>
+          <UserButton afterSignOutUrl="/" />
+        </SignedIn>
+      </nav>
+    </header>
   )
 }
 
-export default App
+export default Header
--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,19 @@ import App from "./App.jsx"
 import "./index.css"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import { ClerkProvider } from "@clerk/clerk-react"
+
+const clerkPubKey = import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <ClerkProvider publishableKey={clerkPubKey}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </ClerkProvider>
   </React.StrictMode>
 )
