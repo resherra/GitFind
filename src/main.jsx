9e87b300c7c3044d6ebbf4930d61250dd0ac64f2
lazych <redouancherrat@gmail.com>
2023-06-22 19:45:40 +0100

    s

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,10 @@ import "./index.css"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { BrowserRouter } from "react-router-dom"
+import axios from "axios"
+
+axios.defaults.baseURL = "https://api.github.com"
+axios.defaults.headers.common["Authorization"] = "Bearer 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"
 
 const queryClient = new QueryClient()
 
@@ -12,9 +16,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <div className={`bg-red-400`}>
-          <App />
-        </div>
+        <App />
       </BrowserRouter>
       <ReactQueryDevtools />
     </QueryClientProvider>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,46 @@
+import { useQuery, useQueryClient } from "@tanstack/react-query"
+import axios from "axios"
+import { useState } from "react"
+
 export default function Home() {
-  return <h1>home</h1>
+  const [user, setUser] = useState("")
+
+  const userQuery = useQuery(
+    ["user", "repos"],
+    async () => {
+      const res = await axios.get(`/users/${user}`)
+
+      return res.data
+    },
+    {
+      enabled: false,
+      staleTime: Infinity,
+    }
+  )
+
+  return (
+    <>
+      <h1>welcode to GiiitFing</h1>
+      <h3>search for a user, navigate repos...</h3>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+          userQuery.refetch()
+        }}
+      >
+        <label htmlFor="search">search for a user:</label>
+        <input type="text" onChange={(e) => setUser(e.target.value)} />
+        <button type="submit">search</button>
+      </form>
+
+      {userQuery.fetchStatus === "idle" && userQuery.isLoading ? null : userQuery.isLoading ? (
+        "loading..."
+      ) : (
+        <>
+          <div>{userQuery.data.login}</div>
+          <div>{userQuery.data.bio}</div>
+        </>
+      )}
+    </>
+  )
 }
