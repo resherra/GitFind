31ee8f6492ac589979797dae1a655d9fa640c1cc
lazych <redouancherrat@gmail.com>
2023-07-02 03:01:57 +0100

    the new pattern works well

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,52 +1,13 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query"
-import axios from "axios"
 import { useState } from "react"
-import { Link } from "react-router-dom"
-import RepoCard from "../components/RepoCard"
-import User from "../components/User"
-import { useEffect } from "react"
-import Pagination from "../components/Pagination"
 import { Routes, Route } from "react-router-dom"
 import Repos from "./Repos"
 import { useNavigate } from "react-router-dom"
+
 export default function Home() {
-  //search value
   const navigate = useNavigate()
   const [user, setUser] = useState("")
   const [page, setPage] = useState(1)
-  const [sent, setSent] = useState(false)
-  const [empty, setEmpty] = useState(false)
-  const queryClient = useQueryClient()
 
-  //fetch function
-  const fetchRepos = async (user, page) => {
-    if (user !== "") {
-      const res = await axios.get(`/users/${user}/repos?per_page=6&page=${page}`)
-      return res.data
-    }
-
-    return 0
-  }
-
-  //query
-  const reposQuery = useQuery(["user", user, "repos", { page }], () => fetchRepos(user, page), {
-    enabled: sent || page !== 1,
-    keepPreviousData: true,
-  })
-
-  useEffect(() => {
-    async function prefetchNext() {
-      await queryClient.prefetchQuery(["user", user, "repos", { page: page + 1 }], () => fetchRepos(user, page + 1))
-      const nextPage = await queryClient.getQueryData(["user", user, "repos", { page: page + 1 }], { exact: true })
-      if (nextPage.length === 0) {
-        setEmpty(true)
-      }
-    }
-
-    prefetchNext()
-  }, [page, queryClient])
-
-  //body
   return (
     <>
       <div className="flex flex-col">
@@ -58,10 +19,8 @@ export default function Home() {
           <form
             onSubmit={async (e) => {
               e.preventDefault()
-              setSent(true)
-              await reposQuery.refetch()
-              setSent(false)
               navigate(`/${user}`)
+              setPage(1)
             }}
             className="flex flex-row justify-between"
           >
@@ -73,33 +32,8 @@ export default function Home() {
         </div>
 
         <Routes>
-          <Route path="/:username" element={<Repos user={user} />} />
+          <Route path="/:username" element={<Repos page={page} setPage={setPage} />} />
         </Routes>
-
-        {/* repos */}
-        {/* {reposQuery.fetchStatus === "idle" && reposQuery.isLoading ? null : reposQuery.isLoading ? (
-          "loading..."
-        ) : (
-          <div className="flex gap-36">
-            <User username={reposQuery.data[1]?.owner.login} userAvatar={reposQuery.data[1]?.owner.avatar_url} />
-            <ul className="w-full">
-              {
-                <li>
-                  <div className="flex flex-col gap-8">
-                    {reposQuery.data.map((repo) => (
-                      <Link key={repo.node_id} to={`/${repo.owner.login}/${repo.name}`}>
-                        <RepoCard reponame={repo.name} />
-                      </Link>
-                    ))}{" "}
-                  </div>
-                </li>
-              }
-            </ul>
-          </div>
-        )} */}
-
-        {/* pagination */}
-        {reposQuery.data ? <Pagination page={page} setPage={setPage} empty={empty} setEmpty={setEmpty} /> : null}
       </div>
     </>
   )
--- src/pages/Repos.jsx
+++ src/pages/Repos.jsx
@@ -1,7 +1,43 @@
-export default function Repos({ user }) {
+import { useQuery, useQueryClient } from "@tanstack/react-query"
+import { useParams, Link } from "react-router-dom"
+import User from "../components/User"
+import { useState } from "react"
+import { useEffect } from "react"
+import axios from "axios"
+import RepoCard from "../components/RepoCard"
+import Pagination from "../components/Pagination"
+
+export default function Repos({ page, setPage }) {
+  const { username } = useParams()
+  const [empty, setEmpty] = useState(false)
+  const queryClient = useQueryClient()
+
+  //query function
+  const fetchRepos = async (user, page) => {
+    const res = await axios.get(`/users/${user}/repos?per_page=6&page=${page}`)
+    return res.data
+  }
+
+  //query
+  const reposQuery = useQuery(["user", username, "repos", { page }], () => fetchRepos(username, page), {
+    keepPreviousData: true,
+  })
+
+  useEffect(() => {
+    async function prefetchNext() {
+      await queryClient.prefetchQuery(["user", username, "repos", { page: page + 1 }], () => fetchRepos(username, page + 1))
+      const nextPage = await queryClient.getQueryData(["user", username, "repos", { page: page + 1 }], { exact: true })
+      if (nextPage.length === 0) {
+        setEmpty(true)
+      }
+    }
+
+    prefetchNext()
+  }, [page, queryClient])
+
   return (
     <>
-      {/* {reposQuery.fetchStatus === "idle" && reposQuery.isLoading ? null : reposQuery.isLoading ? (
+      {reposQuery.fetchStatus === "idle" && reposQuery.isLoading ? null : reposQuery.isLoading ? (
         "loading..."
       ) : (
         <div className="flex gap-36">
@@ -10,7 +46,7 @@ export default function Repos({ user }) {
             {
               <li>
                 <div className="flex flex-col gap-8">
-                  {reposQuery.data.map((repo) => (
+                  {reposQuery.data?.map((repo) => (
                     <Link key={repo.node_id} to={`/${repo.owner.login}/${repo.name}`}>
                       <RepoCard reponame={repo.name} />
                     </Link>
@@ -20,8 +56,8 @@ export default function Repos({ user }) {
             }
           </ul>
         </div>
-      )} */}
-      <h2>this is repos list by {user}</h2>
+      )}
+      {reposQuery.data ? <Pagination page={page} setPage={setPage} empty={empty} setEmpty={setEmpty} /> : null}
     </>
   )
 }
